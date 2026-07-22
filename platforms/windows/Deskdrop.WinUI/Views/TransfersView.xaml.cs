d7b11ef15b067bd2c9292eddee1bdd43a0d8743c
Chinmay Kudalkar <chinmaykudalkarrr@gmail.com>
2026-07-22 16:15:46 +0530

    feat(winui): implement Transfers screen with live active transfers progress, history, and approval actions

--- platforms/windows/Deskdrop.WinUI/Views/TransfersView.xaml.cs
+++ platforms/windows/Deskdrop.WinUI/Views/TransfersView.xaml.cs
@@ -1,12 +1,39 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Deskdrop.WinUI.Views
 {
     public sealed partial class TransfersView : Page
     {
+        public DeskdropStore mgr => DeskdropStore.Shared;
+
         public TransfersView()
         {
             this.InitializeComponent();
+        }
+
+        private void OnAcceptClicked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement)?.DataContext is FileTransferState transfer)
+            {
+                mgr.AcceptTransfer(transfer.transfer_id);
+            }
+        }
+
+        private void OnRejectClicked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement)?.DataContext is FileTransferState transfer)
+            {
+                mgr.RejectTransfer(transfer.transfer_id);
+            }
+        }
+
+        private void OnCancelClicked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement)?.DataContext is FileTransferState transfer)
+            {
+                DaemonClient.CancelFileTransfer(transfer.transfer_id);
+            }
         }
     }
 }
