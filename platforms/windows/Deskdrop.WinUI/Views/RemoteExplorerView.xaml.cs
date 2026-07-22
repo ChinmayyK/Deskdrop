ea1ecc7041e0b93caa3854b0ffe3449314b6d3e5
Chinmay Kudalkar <chinmaykudalkarrr@gmail.com>
2026-07-22 12:22:17 +0530

    ui(winui): implement exact 1:1 macOS Remote Explorer (Browse Device) screen with library sub-sidebar and file grid

---
+++ platforms/windows/Deskdrop.WinUI/Views/RemoteExplorerView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Deskdrop.WinUI.Views
+{
+    public sealed partial class RemoteExplorerView : Page
+    {
+        public RemoteExplorerView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
